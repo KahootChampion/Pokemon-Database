bdc252a0ff6a74faa82bc380cf1d5bc75600e8b1
Haris Muhammad <harismuhammad@Hariss-MacBook-Pro.local>
2023-02-25 18:21:43 -0700

    Type component completed

--- src/Components/Type_Component.tsx
+++ src/Components/Type_Component.tsx
@@ -4,7 +4,44 @@ import { capitalizeWord } from "../Utils/TextUtils/TextUtils";
 
 type IType_Component = {
   firstType: string;
-  secondType?: string;
+  secondType?: string | undefined;
+};
+
+interface ITypeColorMap {
+  [key: string]: string;
+}
+
+const typeColorMap: ITypeColorMap = {
+  normal: "#A8A878",
+  fire: "#F08030",
+  water: "#6890F0",
+  electric: "#F8D030",
+  grass: "#78C850",
+  ice: "#98D8D8",
+  fighting: "#C03028",
+  poison: "#A040A0",
+  ground: "#E0C068",
+  flying: "#A890F0",
+  psychic: "#F85888",
+  bug: "#A8B820",
+  rock: "#B8A038",
+  ghost: "#705898",
+  dragon: "#7038F8",
+  dark: "#705848",
+  steel: "#B8B8D0",
+  fairy: "#EE99AC",
+};
+
+const getTypeColorMapping = (
+  typeName: string | undefined
+): string | undefined => {
+  const color: string | undefined = typeName
+    ? typeColorMap[typeName]
+    : undefined;
+  if (color) {
+    return color;
+  }
+  return undefined;
 };
 
 export default function Type_Component({
@@ -12,9 +49,15 @@ export default function Type_Component({
   secondType,
 }: IType_Component) {
   return (
-    <h4>
-      {capitalizeWord(firstType)} {secondType ? <span>*</span> : null}{" "}
-      {secondType ? capitalizeWord(secondType) : null}
-    </h4>
+    <h5 className="type-styling">
+      <span style={{ color: getTypeColorMapping(firstType) }}>
+        {" "}
+        {capitalizeWord(firstType)}
+      </span>{" "}
+      {secondType ? <span>·</span> : null}{" "}
+      <span style={{ color: getTypeColorMapping(secondType) }}>
+        {secondType ? capitalizeWord(secondType) : null}
+      </span>{" "}
+    </h5>
   );
 }
