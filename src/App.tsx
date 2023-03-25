7b1cdca1157a5a3ff43f543644ce5e9715870638
Haris Muhammad <harismuhammad@Hariss-MacBook-Pro.local>
2023-03-25 13:50:59 -0600

    Removed shared layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import PokemonInfo from "./Pages/PokemonInfo/PokemonInfo";
-import Layout from "./Pages/Shared/LayoutComponent";
 
 function App() {
   return (
-    <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/pokemon/:id" element={<PokemonInfo />} />
       </Routes>
-    </Layout>
   );
 }
 
